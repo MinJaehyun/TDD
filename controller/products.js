d255032bd78b35df789af8b17e393b7d91022a3c
{krism01} <{krism01@naver.com}>
2022-02-07 19:32:56 +0900

    update toBeCalledWith

--- controller/products.js
+++ controller/products.js
@@ -3,6 +3,9 @@ const productModel = require('../models/product');
 exports.createProduct = (req, res, next) => {
   // model.Article 이런식으로 사용했다.
   productModel.create(req.body);
+  // 아래처럼 사용 가능
+  // const info = req.body;     // name, descripttion, price
+  // productModel.create(info);
 
   // res.send("test");
   // TypeError: Cannot read properties of undefined (reading 'send')
--- test/unit/products.test.js
+++ test/unit/products.test.js
@@ -17,8 +17,10 @@ describe("Product Controller Create", () => {
   })
   it("should call ProductModel.create", () => {
     productController.createProduct(req, res, next);
+    // toBeCalledWith: 인자로 무엇이 넘어 왔는지를 검증할 수 있다.
     expect(productModel.create).toBeCalledWith(newProduct);
     // toBeCalled : 잘못된 Matcher 사용 시, 아래 에러 발생
     // Matcher error: this matcher must not have an expected argument
+
   })
 })
