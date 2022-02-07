111c28d6f0ef1f4f5eac23886455560a476bf29f
{krism01} <{krism01@naver.com}>
2022-02-07 15:55:14 +0900

    mvc 설정

--- index.js
+++ index.js
@@ -1,14 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const productRouters = require('./routes.js');
 
-const PORT = 5000;
+// Constants
+const PORT = 8080;
 
+// App
 const app = express();
 
+require('dotenv').config()
+
 app.use(express.json());
+
+mongoose.connect(`mongodb+srv://${process.env.DB_ID}:${process.env.DB_PASSWORD}@cluster0.gnquy.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`)
+  .then(() => console.log(`MongoDB Connected...`))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
 
-app.listen(PORT);
-console.log(`Running on port ${PORT}`);
+app.use("/api/products", productRouters)
+
+app.listen(PORT, () => {
+  console.log(`Running on port ${PORT}`);
+});
