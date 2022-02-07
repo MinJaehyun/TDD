7050bd7c40ec39bf68701c69761f441a82592408
{krism01} <{krism01@naver.com}>
2022-02-07 16:18:43 +0900

    test function test articleModel.create 를 호출하는지

--- controller/products.js
+++ controller/products.js
@@ -1,3 +1,9 @@
-exports.hello = (req, res) => {
-  res.send("hi")
+const productModel = require('../models/product');
+
+exports.createProduct = (req, res) => {
+  // model.Article 이런식으로 사용했다.
+  productModel.create();
+
+  // res.send("test");
+  // TypeError: Cannot read properties of undefined (reading 'send')
 };
