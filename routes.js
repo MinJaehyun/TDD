e4abf0aebc6091785a6881928890761ed2c7b371
{krism01} <{krism01@naver.com}>
2022-02-09 00:23:29 +0900

    int productDetail

--- routes.js
+++ routes.js
@@ -26,6 +26,7 @@ Jest did not exit one second after the test run has completed.
 This usually means that there are asynchronous operations that weren't stopped in your tests. Consider running Jest with `--detectOpenHandles` to troubleshoot this issue.  
  */
 
-router.get('/', productController.getProducts)
+router.get('/', productController.getProducts);
+router.get('/:productId', productController.getProductById);
 
 module.exports = router;
--- test/integration/product.int.test.js
+++ test/integration/product.int.test.js
@@ -1,40 +1,48 @@
 const request = require('supertest');
 const app = require('../../index');
 const newProduct = require('../data/new-product.json');
-// console.log('newProduct: ', newProduct);
-// const newProduct = require('../data/new-product.json')
+let firstProduct;
 
-describe("Product Integration Create", () => {
-  it("POST /api/products", async () => {
-    const response = await request(app)
-      .post("/api/products")
-      .send(newProduct);
-    // console.log('response: ', response);
-    expect(response.statusCode).toBe(201)
-    expect(response.body.name).toBe(newProduct.name)
-    expect(response.body.description).toBe(newProduct.description)
-  })
-
-  it("should return 500 on POST /api/products", async () => {
-    const response = await request(app)
-      .post('/api/products')
-      // 에러: connect ECONNREFUSED 127.0.0.1:80
-      // 해결: .post('api/products') 를 .post('/api/products')
-      .send({ name: "phone" });
-    expect(response.statusCode).toBe(500)
-    // console.log('response.body', response.body);
-    // expect(response.body).toStrictEqual({ message: "" })
-    // 콘솔에 찍힌 에러를 넣어준다.
-    expect(response.body).toStrictEqual({ message: "Product validation failed: description: Path `description` is required." })
-  })
+it("POST /api/products", async () => {
+  const response = await request(app)
+    .post("/api/products")
+    .send(newProduct);
+  // console.log('response: ', response);
+  expect(response.statusCode).toBe(201)
+  expect(response.body.name).toBe(newProduct.name)
+  expect(response.body.description).toBe(newProduct.description)
 })
 
-describe("Product Integration Read", () => {
-  it("GET /api/products", async () => {
-    const response = await request(app).get('/api/products');
-    expect(response.statusCode).toBe(200);
-    expect(Array.isArray(response.body)).toBeTruthy();
-    expect(response.body[0].name).toBeDefined();
-    expect(response.body[0].description).toBeDefined();
-  })
+it("should return 500 on POST /api/products", async () => {
+  const response = await request(app)
+    .post('/api/products')
+    // 에러: connect ECONNREFUSED 127.0.0.1:80
+    // 해결: .post('api/products') 를 .post('/api/products')
+    .send({ name: "phone" });
+  expect(response.statusCode).toBe(500)
+  // console.log('response.body', response.body);
+  // expect(response.body).toStrictEqual({ message: "" })
+  // 콘솔에 찍힌 에러를 넣어준다.
+  expect(response.body).toStrictEqual({ message: "Product validation failed: description: Path `description` is required." })
+})
+
+it("GET /api/products", async () => {
+  const response = await request(app).get('/api/products');
+  expect(response.statusCode).toBe(200);
+  expect(Array.isArray(response.body)).toBeTruthy();
+  expect(response.body[0].name).toBeDefined();
+  expect(response.body[0].description).toBeDefined();
+  firstProduct = response.body[0];
+})
+
+it("GET /api/products/:productId", async () => {
+  const response = await request(app).get('/api/products/' + firstProduct._id);
+  expect(response.statusCode).toBe(200);
+  expect(response.body.name).toBe(firstProduct.name);
+  expect(response.body.description).toBe(firstProduct.description);
+})
+
+it("GET id doesn't exitst /api/products/:productId", async () => {
+  const response = await request(app).get('/api/products/6201ef110260a022ede7d111');
+  expect(response.statusCode).toBe(404);
 })
