6008ed31cc56c5032f2047f2d02dae8adc314bb8
{krism01} <{krism01@naver.com}>
2022-02-08 18:56:25 +0900

    jest 기본 환경이 jsdom 에서 node 로 변경됨으로 환경 설정 제거

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,6 @@
-module.exports = {
-  testEnvironment: "node"
-}
+// Jest 27: New Defaults for Jest, 2021 edition
+// we are changing the default test environment from "jsdom" to "node"
+
+// module.exports = {
+//   testEnvironment: "node"
+// }
